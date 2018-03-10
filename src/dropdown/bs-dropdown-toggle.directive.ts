30d804512b516a99f7665b398a7e94d611a48ca2
Dan Fearing <dan.fearing@viewpoint.com>
2018-03-10 11:55:07 -0800

    Match naming convention.

--- src/dropdown/bs-dropdown-toggle.directive.ts
+++ src/dropdown/bs-dropdown-toggle.directive.ts
@@ -25,9 +25,9 @@ export class BsDropdownToggleDirective implements OnDestroy {
   @HostBinding('attr.aria-expanded') isOpen: boolean;
 
   private _subscriptions: Subscription[] = [];
-  private scrollListenerCancellationFn: Function;
-  private clickListenerCancellationFn: Function;
-  private keyupListenerCancellationFn: Function;
+  private _scrollListenerCancellationFn: Function;
+  private _clickListenerCancellationFn: Function;
+  private _keyupListenerCancellationFn: Function;
 
   constructor(private _state: BsDropdownState, private _element: ElementRef, private _renderer: Renderer2) {
     // sync is open value with state
@@ -63,14 +63,14 @@ export class BsDropdownToggleDirective implements OnDestroy {
   }
 
   attachListeners() {
-    if (!this.scrollListenerCancellationFn) {
-      this.scrollListenerCancellationFn = this._renderer.listen("window", "scroll", () => this._state.toggleClick.emit(false));
+    if (!this._scrollListenerCancellationFn) {
+      this._scrollListenerCancellationFn = this._renderer.listen("window", "scroll", () => this._state.toggleClick.emit(false));
     }
-    if (!this.keyupListenerCancellationFn) {
-      this.keyupListenerCancellationFn = this._renderer.listen("document", "keyup.esc", () => this._state.toggleClick.emit(false));
+    if (!this._keyupListenerCancellationFn) {
+      this._keyupListenerCancellationFn = this._renderer.listen("document", "keyup.esc", () => this._state.toggleClick.emit(false));
     }
-    if (!this.clickListenerCancellationFn) {
-      this.clickListenerCancellationFn = this._renderer.listen("document", "click", (event: any) => {
+    if (!this._clickListenerCancellationFn) {
+      this._clickListenerCancellationFn = this._renderer.listen("document", "click", (event: any) => {
         if (event.button !== 2 && !this._element.nativeElement.contains(event.target)) {
           this._state.toggleClick.emit(false);
         }
@@ -79,17 +79,17 @@ export class BsDropdownToggleDirective implements OnDestroy {
   }
 
   removeListeners(): void {
-    if (this.scrollListenerCancellationFn) {
-      this.scrollListenerCancellationFn();
-      this.scrollListenerCancellationFn = null;
+    if (this._scrollListenerCancellationFn) {
+      this._scrollListenerCancellationFn();
+      this._scrollListenerCancellationFn = null;
     }
-    if (this.clickListenerCancellationFn) {
-      this.clickListenerCancellationFn();
-      this.clickListenerCancellationFn = null;
+    if (this._clickListenerCancellationFn) {
+      this._clickListenerCancellationFn();
+      this._clickListenerCancellationFn = null;
     }
-    if (this.keyupListenerCancellationFn) {
-      this.keyupListenerCancellationFn();
-      this.keyupListenerCancellationFn = null;
+    if (this._keyupListenerCancellationFn) {
+      this._keyupListenerCancellationFn();
+      this._keyupListenerCancellationFn = null;
     }
   }
 }
