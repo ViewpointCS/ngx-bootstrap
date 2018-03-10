c3927e6fb313a7e54da216bd93ad20afb69c6358
Dan Fearing <dan.fearing@viewpoint.com>
2018-03-10 09:31:49 -0800

    Fixing performance problem with drop down.

--- src/dropdown/bs-dropdown-toggle.directive.ts
+++ src/dropdown/bs-dropdown-toggle.directive.ts
@@ -4,7 +4,8 @@ import {
   HostBinding,
   HostListener,
   Input,
-  OnDestroy
+  OnDestroy,
+  Renderer2
 } from '@angular/core';
 import { Subscription } from 'rxjs/Subscription';
 
@@ -24,8 +25,11 @@ export class BsDropdownToggleDirective implements OnDestroy {
   @HostBinding('attr.aria-expanded') isOpen: boolean;
 
   private _subscriptions: Subscription[] = [];
+  private scrollListenerCancellationFn: Function;
+  private clickListenerCancellationFn: Function;
+  private keyupListenerCancellationFn: Function;
 
-  constructor(private _state: BsDropdownState, private _element: ElementRef) {
+  constructor(private _state: BsDropdownState, private _element: ElementRef, private _renderer: Renderer2) {
     // sync is open value with state
     this._subscriptions.push(
       this._state.isOpenChange.subscribe(
@@ -38,6 +42,10 @@ export class BsDropdownToggleDirective implements OnDestroy {
         (value: boolean) => (this.isDisabled = value || null)
       )
     );
+
+    if (this._state.autoClose) {
+      this.attachListeners();
+    }
   }
 
   @HostListener('click', [])
@@ -48,27 +56,41 @@ export class BsDropdownToggleDirective implements OnDestroy {
     this._state.toggleClick.emit(true);
   }
 
-  @HostListener('document:click', ['$event'])
-  onDocumentClick(event: any): void {
-    if (
-      this._state.autoClose &&
-      event.button !== 2 &&
-      !this._element.nativeElement.contains(event.target)
-    ) {
-      this._state.toggleClick.emit(false);
-    }
-  }
-
-  @HostListener('keyup.esc')
-  onEsc(): void {
-    if (this._state.autoClose) {
-      this._state.toggleClick.emit(false);
-    }
-  }
-
   ngOnDestroy(): void {
+    this.removeListeners();
     for (const sub of this._subscriptions) {
       sub.unsubscribe();
+    }
+  }
+
+  attachListeners() {
+    if (!this.scrollListenerCancellationFn) {
+      this.scrollListenerCancellationFn = this._renderer.listen("window", "scroll", () => this._state.toggleClick.emit(false));
+    }
+    if (!this.keyupListenerCancellationFn) {
+      this.keyupListenerCancellationFn = this._renderer.listen("document", "keyup.esc", () => this._state.toggleClick.emit(false));
+    }
+    if (!this.clickListenerCancellationFn) {
+      this.clickListenerCancellationFn = this._renderer.listen("document", "click", (event: any) => {
+        if (event.button !== 2 && !this._element.nativeElement.contains(event.target)) {
+          this._state.toggleClick.emit(false);
+        }
+      });
+    }
+  }
+
+  removeListeners(): void {
+    if (this.scrollListenerCancellationFn) {
+      this.scrollListenerCancellationFn();
+      this.scrollListenerCancellationFn = null;
+    }
+    if (this.clickListenerCancellationFn) {
+      this.clickListenerCancellationFn();
+      this.clickListenerCancellationFn = null;
+    }
+    if (this.keyupListenerCancellationFn) {
+      this.keyupListenerCancellationFn();
+      this.keyupListenerCancellationFn = null;
     }
   }
 }
